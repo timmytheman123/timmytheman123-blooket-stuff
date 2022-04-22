175f6e4f142eeafcdf828fde6353fd1975e68d39
Tiimmytheman123 <91916360+timmytheman123@users.noreply.github.com>
2022-04-22 08:29:57 -0400

    Update gui.js

--- blooket utility injectables/glixzzy/gui.js
+++ blooket utility injectables/glixzzy/gui.js
@@ -1,3 +1,23 @@
+/* credit to zastix for this bypass */
+window.alert = function(str) {
+    bypass = document.body.appendChild(document.createElement("iframe")).contentWindow.alert;
+    bypass(str)
+}
+window.prompt = function(str, s) {
+    bypass = document.body.appendChild(document.createElement("iframe")).contentWindow.prompt;
+    res = bypass(str, s)
+    return res;
+}
+window.confirm = function(str) {
+    bypass = document.body.appendChild(document.createElement("iframe")).contentWindow.confirm;
+    res = bypass(str)
+    return res;
+}
+setInterval(() => {
+    if (document.getElementsByTagName("iframe")[1] != null) {
+        document.getElementsByTagName("iframe")[1].remove();
+    }
+}, 10);
 alert("Working")
 blooketUtility.api.gui.glixzzy = {
   title: "Glixzzy",
