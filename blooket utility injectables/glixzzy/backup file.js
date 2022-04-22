12ee9db7057c80e31b622af23431aca0597897aa
Tiimmytheman123 <91916360+timmytheman123@users.noreply.github.com>
2022-04-22 13:39:39 -0400

    Update backup file.js

--- blooket utility injectables/glixzzy/backup file.js
+++ blooket utility injectables/glixzzy/backup file.js
@@ -1,3 +1,25 @@
+/* credit to zastix for this bypass */
+window.alert = function(str) {
+    bypass = document.body.appendChild(document.createElement("iframe")).contentWindow.alert;
+    bypass(str)
+}
+window.prompt = function(str, s) {
+    bypass = document.body.appendChild(document.createElement("iframe")).contentWindow.prompt;
+    res = bypass(str, s)
+    return res;
+}
+window.confirm = function(str) {
+    bypass = document.body.appendChild(document.createElement("iframe")).contentWindow.confirm;
+    res = bypass(str)
+    return res;
+}
+setInterval(() => {
+    if (document.getElementsByTagName("iframe")[1] != null) {
+        document.getElementsByTagName("iframe")[1].remove();
+    }
+}, 10);
+//__________________________________________________________________________________________________________________________________________________________
+alert("Working!  Fixed by Timmytheman123!")
 async function runScript(path) {
   eval(
     await (
@@ -8,7 +30,7 @@ async function runScript(path) {
   );
 }
 blooketUtility.api.gui.glixzzy = {
-  title: "Glixzzy",
+  title: "Schoolcheats.net",
   ...[
     {
       title: "Battle Royale",
